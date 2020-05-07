30db92c98bb57988dac9a8d9acc7a3a45ca341c8
Anthony Leedom <anthonym.leedom@gmail.com>
2020-05-07 16:04:49 -0500

    add reverse::all_of/any_of/none_of and tests

--- include/halg.hpp
+++ include/halg.hpp
@@ -71,7 +71,7 @@ namespace reverse {
 template <typename UnaryFunction, typename... Elements>
 void for_each(UnaryFunction&& func, Elements&&... elements)
 {
-    int foo;
+    auto foo = 0;
     (foo = ... = (func(elements), 0));
 }
 
@@ -79,7 +79,63 @@ template <typename UnaryFunction>
 auto for_each(UnaryFunction&& func)
 {
     return [func = std::forward<UnaryFunction>(func)](auto&&... elements) {
-        return for_each(func, std::forward<decltype(elements)>(elements)...);
+        return reverse::for_each(func,
+                                 std::forward<decltype(elements)>(elements)...);
+    };
+}
+
+/* --------------------------------- all_of --------------------------------- */
+template <typename UnaryPredicate, typename... Elements>
+auto all_of(UnaryPredicate&& predicate, Elements&&... elements) -> bool
+{
+    auto x = true;
+    int foo;
+    (foo = ... = (x = x && predicate(std::forward<Elements>(elements))));
+    return x;
+}
+
+template <typename UnaryPredicate>
+auto all_of(UnaryPredicate&& predicate)
+{
+    return [f = std::forward<UnaryPredicate>(predicate)](auto&&... elements) {
+        return reverse::all_of(f,
+                               std::forward<decltype(elements)>(elements)...);
+    };
+}
+
+/* --------------------------------- any_of --------------------------------- */
+template <typename UnaryPredicate, typename... Elements>
+auto any_of(UnaryPredicate&& predicate, Elements&&... elements) -> bool
+{
+    auto x = false;
+    int foo;
+    (foo = ... = (x = x || predicate(std::forward<Elements>(elements))));
+    return x;
+}
+
+template <typename UnaryPredicate>
+auto any_of(UnaryPredicate&& predicate)
+{
+    return [f = std::forward<UnaryPredicate>(predicate)](auto&&... elements) {
+        return reverse::any_of(f,
+                               std::forward<decltype(elements)>(elements)...);
+    };
+}
+
+/* -------------------------------- none_of --------------------------------- */
+template <typename UnaryPredicate, typename... Elements>
+auto none_of(UnaryPredicate&& predicate, Elements&&... elements) -> bool
+{
+    return !reverse::any_of(std::forward<UnaryPredicate>(predicate),
+                            std::forward<Elements>(elements)...);
+}
+
+template <typename UnaryPredicate>
+auto none_of(UnaryPredicate&& predicate)
+{
+    return [f = std::forward<UnaryPredicate>(predicate)](auto&&... elements) {
+        return reverse::none_of(f,
+                                std::forward<decltype(elements)>(elements)...);
     };
 }
 
--- tests/all_any_none_of.test.cpp
+++ tests/all_any_none_of.test.cpp
@@ -1,3 +1,5 @@
+#include <sstream>
+
 #include <catch2/catch.hpp>
 
 #include <halg.hpp>
@@ -24,6 +26,45 @@ TEST_CASE("all_of", "[HALG]")
 
         CHECK(all_are_gt_42(54.3, 100.23, 'A'));
         CHECK(!all_are_gt_42(54.3, -1, 'A'));
+    }
+
+    SECTION("reverse")
+    {
+        auto ss = std::stringstream{};
+        CHECK(!halg::reverse::all_of(
+            [&](auto x) {
+                ss << x;
+                return x > 5;
+            },
+            1, 4, 6, 'a'));
+        CHECK(ss.str() == "a64");
+
+        CHECK(
+            halg::reverse::all_of([](auto x) { return x; }, true, true, true));
+        CHECK(!halg::reverse::all_of([](auto x) { return x; }, true, true,
+                                     false));
+        CHECK(!halg::reverse::all_of([](auto x) { return x; }, false, false));
+        CHECK(halg::reverse::all_of([](auto x) { return x; }, true));
+        CHECK(!halg::reverse::all_of([](auto x) { return x; }, false));
+    }
+
+    SECTION("reverse and bound")
+    {
+        auto ss               = std::stringstream{};
+        auto all_are_gt_5_rev = halg::reverse::all_of([&](auto x) {
+            ss << x;
+            return x > 5;
+        });
+
+        CHECK(!all_are_gt_5_rev(1, 4, 6, 'a', 100.5));
+        CHECK(ss.str() == "100.5a64");
+
+        auto all_are_even_rev =
+            halg::reverse::all_of([](auto x) { return x % 2 == 0; });
+
+        CHECK(!all_are_even_rev(2, 4, 12, true, 2));
+        CHECK(all_are_even_rev(2, 4, 12, false, 2));
+        CHECK(all_are_even_rev(2));
     }
 }
 
@@ -53,6 +94,46 @@ TEST_CASE("any_of", "[HALG]")
         CHECK(any_are_gt_42(54.3, 0.4, 'A'));
         CHECK(!any_are_gt_42(4.3, -1, 5.f));
     }
+
+    SECTION("reverse")
+    {
+        auto ss = std::stringstream{};
+        CHECK(halg::reverse::any_of(
+            [&](auto x) {
+                ss << x;
+                return x > 5;
+            },
+            1, 4, 6, 'a', 0, -5.4, 2));
+        CHECK(ss.str() == "2-5.40a");
+
+        CHECK(
+            halg::reverse::any_of([](auto x) { return x; }, true, true, true));
+        CHECK(!halg::reverse::any_of([](auto x) { return x; }, false, false,
+                                     false));
+        CHECK(!halg::reverse::any_of([](auto x) { return x; }, false, false));
+        CHECK(halg::reverse::any_of([](auto x) { return x; }, true));
+        CHECK(!halg::reverse::any_of([](auto x) { return x; }, false));
+    }
+
+    SECTION("reverse and bound")
+    {
+        auto ss               = std::stringstream{};
+        auto any_are_gt_5_rev = halg::reverse::any_of([&](auto x) {
+            ss << x;
+            return x > 5;
+        });
+
+        CHECK(any_are_gt_5_rev(1, 4, 6, 'a', 100.5, 0, -2.4, 2));
+        CHECK(ss.str() == "2-2.40100.5");
+
+        auto any_are_even_rev =
+            halg::reverse::any_of([](auto x) { return x % 2 == 0; });
+
+        CHECK(any_are_even_rev(2, 3, 12, true, 2));
+        CHECK(any_are_even_rev(3, 7, 1, true, 2));
+        CHECK(!any_are_even_rev(3, 7, 1, true));
+        CHECK(!any_are_even_rev()()()()()()()()(1));
+    }
 }
 
 TEST_CASE("none_of", "[HALG]")
@@ -80,5 +161,45 @@ TEST_CASE("none_of", "[HALG]")
         CHECK(!none_are_gt_42(54.3, 100.23, 'A'));
         CHECK(!none_are_gt_42(54.3, 0.4, 'A'));
         CHECK(none_are_gt_42(4.3, -1, 5.f));
+    }
+
+    SECTION("reverse")
+    {
+        auto ss = std::stringstream{};
+        CHECK(!halg::reverse::none_of(
+            [&](auto x) {
+                ss << x;
+                return x > 5;
+            },
+            1, 4, 6, 'a', 0, -5.4, 2));
+        CHECK(ss.str() == "2-5.40a");
+
+        CHECK(!halg::reverse::none_of([](auto x) { return x; }, true, true,
+                                      true));
+        CHECK(halg::reverse::none_of([](auto x) { return x; }, false, false,
+                                     false));
+        CHECK(halg::reverse::none_of([](auto x) { return x; }, false, false));
+        CHECK(!halg::reverse::none_of([](auto x) { return x; }, true));
+        CHECK(halg::reverse::none_of([](auto x) { return x; }, false));
+    }
+
+    SECTION("reverse and bound")
+    {
+        auto ss                = std::stringstream{};
+        auto none_are_gt_5_rev = halg::reverse::none_of([&](auto x) {
+            ss << x;
+            return x > 5;
+        });
+
+        CHECK(!none_are_gt_5_rev(1, 4, 6, 'a', 100.5, 0, -2.4, 2));
+        CHECK(ss.str() == "2-2.40100.5");
+
+        auto none_are_even_rev =
+            halg::reverse::none_of([](auto x) { return x % 2 == 0; });
+
+        CHECK(!none_are_even_rev(2, 3, 12, true, 2));
+        CHECK(!none_are_even_rev(3, 7, 1, true, 2));
+        CHECK(none_are_even_rev(3, 7, 1, true));
+        CHECK(none_are_even_rev(1));
     }
 }
